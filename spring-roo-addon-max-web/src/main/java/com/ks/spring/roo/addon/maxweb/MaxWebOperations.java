57a869fe451fac2da7187d0b13958d48a2c45a48
yunsunghan@gmail.com <yunsunghan@gmail.com@ad532a8e-a535-8dfa-f283-c8c867a5e721>
2011-06-13 08:04:56 +0000

    최초 입력 (아직 각종 jsp 및 properties는 다듬어지지 않음)

---
+++ spring-roo-addon-max-web/src/main/java/com/ks/spring/roo/addon/maxweb/MaxWebOperations.java
@@ -0,0 +1,28 @@
+package com.ks.spring.roo.addon.maxweb;
+
+import org.springframework.roo.model.JavaType;
+
+/**
+ * Interface of operations this add-on offers. Typically used by a command type or an external add-on.
+ *
+ * @since 1.1
+ */
+public interface MaxWebOperations {
+
+	/**
+	 * Indicate commands should be available
+	 * 
+	 * @return true if it should be available, otherwise false
+	 */
+	boolean isCommandAvailable();
+
+	/**
+	 * Annotate the provided Java type with the trigger of this add-on
+	 */
+	void newMaxWebClass(JavaType webClazz, JavaType serviceClazz);
+	
+	/**
+	 * Setup all add-on artifacts (dependencies in this case)
+	 */
+	void setup();
+}
